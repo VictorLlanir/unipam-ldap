6d7826ea734906fdcb4911e477d8f53af259ee62
Victor Llanir <victortrevisan1997@gmail.com>
2023-01-19 14:36:11 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using LdapForNet;
+try
+{
+    using (var cn = new LdapConnection())
+    {
+        // connect
+        cn.Connect("172.17.15.20", 389);
+        // bind using kerberos credential cache file
+        cn.Bind(userDn: "unipam.edu.br\\brunonoremberg", password: "885848099");
+    }
+}
+catch (Exception e)
+{
+
+	throw;
+}
