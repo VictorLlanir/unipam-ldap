a9bd64db5c762a0ba9b072d5c7fa0d2546d04362
Victor Llanir <victor.llanir@ellevo.com>
2023-01-20 18:32:19 +0000

    Testes

--- Program.cs
+++ Program.cs
@@ -1,16 +1,25 @@
 ﻿using LdapForNet;
+using static LdapForNet.Native.Native;
+
 try
 {
-    using (var cn = new LdapConnection())
+    using (var connection = new LdapConnection())
     {
-        // connect
-        cn.Connect("172.17.15.20", 389);
-        // bind using kerberos credential cache file
-        cn.Bind(userDn: "unipam.edu.br\\brunonoremberg", password: "885848099");
+        connection.Connect("172.17.15.20", 389, LdapSchema.LDAP);
+
+        connection.Bind(LdapAuthType.ExternalAd, new LdapCredential
+        {
+            UserName = "cn=nssldap,ou=people,dc=unipam,dc=edu,dc=br",
+            Password = "60473822",
+        });
+
+        Console.WriteLine("connected");
     }
 }
 catch (Exception e)
 {
+    Console.WriteLine(e.Message);
+    Console.WriteLine("not connected");
 
-	throw;
+    throw;
 }
